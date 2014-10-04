b0ef506d2232b12f01ae17c198e08f60e51921c8
mickey <mickey.a.warner@gmail.com>
2014-10-04 17:18:58 -0600

    yes

--- 651/mini_project/2/code.R
+++ 651/mini_project/2/code.R
@@ -69,26 +69,36 @@ rW = function(n){
         byrow = TRUE)
     }
 
-# estimating G via a grid
-mu.vec = seq(4.0, 9.0, length=75)
-sig.vec = seq(0.15, 3.0, length=75)
-xy.1 = cross(mu.vec, sig.vec)
+# estimating G via a grid (use for plotting)
+mu.vec = seq(4.0, 10.0, length=500)
+sig.vec = seq(0.05, 4.0, length=500)
 
-mu.vec = seq(4.0, 9.0, length=250)
-sig.vec = seq(0.15, 3.0, length=250)
-xy.2 = cross(mu.vec, sig.vec)
+# fine grid around mode (not nice for plotting)
+mu.vec = seq(5.6, 6.0, length=500)
+sig.vec = seq(0.25, 0.30, length=500)
+xy = cross(mu.vec, sig.vec)
 
 nu = 4
 sigma = matrix(c(0.24, 0.17, 0.17, 0.23), 2, 2)
-sigma = sigma/3.5
-z = log(apply(xy.2, 1, g))
-w = log(apply(xy.1, 1, dW))
+sigma = sigma/4.589
+z = log(apply(xy, 1, g))
+w = log(apply(xy, 1, dW))
 (G = max(z - w))
 
+xy[which.max(z-w),]
+
+niter = 10000
+system.time(X <- reject(niter))
+mean(X[,5]); max(X[,4])
+# remove NA
+#X = X[-which(is.na(X[,5])),]
+# porportion of acceptances (about 0.35 so far)
+mean(X[,5])
+
 z.star = z - G
-plot3d(cbind(xy.1[w > -15,], (w[w > -15])), col=rgb(0.0589,0.4980,0.2812),
+plot3d(cbind(xy[w > -15,], (w[w > -15])), col=rgb(0.0589,0.4980,0.2812),
     xlab = "mu", ylab = "sigma^2", zlab="density")
-points3d(cbind(xy.2[z.star > -15,], (z.star[z.star > -15])),
+points3d(cbind(xy[z.star > -15,], (z.star[z.star > -15])),
     col='dodgerblue')
 
 # take a screenshot of the rgl device
@@ -122,6 +132,8 @@ reject = function(M = 100){
 
 niter = 3500000
 system.time(X <- reject(niter))
+# remove NA
+X = X[-which(is.na(X[,5])),]
 # porportion of acceptances (about 0.35 so far)
 mean(X[,5])
 
@@ -139,11 +151,34 @@ at = which.max(X[,4])
 
 # accepted draws
 Y = X[X[,5] == 1, 1:2]
-calc.mode(density(Y[,1]))
-calc.mode(density(Y[,2]))
+#calc.mode(density(Y[,1]))
+#calc.mode(density(Y[,2]))
+
+apply(Y, 2, mean)
+var(Y)
+apply(Y, 2, sd)
+cor(Y)
+
+preds = rtruncnorm(nrow(Y), 1, 7, Y[,1], sqrt(Y[,2]))
+dens = density(preds)
+
+pdf("figs/pred.pdf")
+plot(dens, xlab = "Score", ylab = "", cex.lab = 1.5, main = "")
+polygon(dens, col='gray')
+lines(c(5,5),c(0,0.28))
+dev.off()
+
+mean(preds >= 5)
 
 # joint posterior
-plot(Y[sample(nrow(Y), 100000),], pch=20)
+pdf("figs/draws.pdf")
+par(mar=c(4,4.9,2,1))
+set.seed(18)
+plot(Y[sample(nrow(Y), 10000),], pch=20, xlab = expression(mu),
+    ylab = expression(sigma^2), cex.lab = 2.0)
+contour(mu.vec, sig.vec, z.mat, col='dodgerblue', lwd=2, add=TRUE)
+dev.off()
+
 
 plot(density(Y[,2]))
 
@@ -154,20 +189,18 @@ plot(density(Y[sample(nrow(Y), 100000),2]))
 plot(density(y))
 curve(dnorm(x, mean(Y[,1]), sqrt(mean(Y[,2]))), add=TRUE, col='blue')
 
+### contour plot
 mu.vec = seq(5.25, 6.5, length=200)
-sig.vec = seq(0.1, 1.25, length=200)
+sig.vec = seq(0, 1.25, length=200)
 z.mat = matrix(0, length(mu.vec), length(sig.vec))
 for (i in 1:length(mu.vec))
     for (j in 1:length(sig.vec))
         z.mat[i,j] = exp(g.star(c(mu.vec[i], sig.vec[j])))
-
-
 pdf("figs/contour.pdf")
-par(mar=c(4,4.5,2,1))
+par(mar=c(4,4.9,2,1))
 contour(mu.vec, sig.vec, z.mat, xlim=c(5.25,6.5), ylim=c(0,1.25),
-    col='dodgerblue', main="Unnormalized posterior",
-    xlab = expression(mu), ylab = expression(sigma^2),
-    cex.lab = 1.5, cex.main=1.5, lwd=2)
+    col='dodgerblue', xlab = expression(mu), cex.main=1.5, lwd=2,
+    ylab = expression(sigma^2), cex.lab = 2.0)
 dev.off()
 
 points(Y, pch=20)
