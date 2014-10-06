ebf68ecd92fdbfba429efe5962ae7444cb4c28a3
mickey <mickey.a.warner@gmail.com>
2014-10-06 16:14:52 -0600

    yes

--- 666/mini_project/2/phil_code.R
+++ 666/mini_project/2/phil_code.R
@@ -97,31 +97,56 @@ astar <- diag(sqrt(diag(Se))) %*% a
 astar 
 
 
+### test for missing at random
+
 miss.mat2 <- 1*is.na(olive2)
 miss.mat4 <- 1*is.na(olive4)
 
 colnames(miss.mat2) <- colnames(miss.mat4) <- c("a1","a2","a3","a4","a5","a6","a7","a8")
 
 ### multinomial test
+calc.chi = function(x, n)
+    sum((x - n/length(x))^2/(n/length(x)))
 miss.mult2 = apply(miss.mat2, 2, sum)
 miss.mult4 = apply(miss.mat4, 2, sum)
 
-tt = dmultinom(miss.mult2, prob = rep(1/p, p))
-vec = apply(rmultinom(1000000, sum(miss.mult2), prob = rep(1/p, p)),
-    2, function(x) dmultinom(x, sum(miss.mult2), rep(1/p, p)))
-mean(vec <= tt)
+tt = calc.chi(miss.mult2, sum(miss.mult2))
+vec = apply(rmultinom(10000, sum(miss.mult2), prob = rep(1/p, p)),
+    2, function(x) calc.chi(x, sum(miss.mult2)))
+hist(vec, col='gray'); abline(v=tt)
+plot(density(vec), type='l'); abline(v = tt)
+mean(vec >= tt)
 
-tt = dmultinom(miss.mult4, prob = rep(1/p, p))
-vec = apply(rmultinom(1000000, sum(miss.mult4), prob = rep(1/p, p)),
-    2, function(x) dmultinom(x, sum(miss.mult4), rep(1/p, p)))
-mean(vec <= tt)
+tt = calc.chi(miss.mult4, sum(miss.mult4))
+vec = apply(rmultinom(10000, sum(miss.mult4), prob = rep(1/p, p)),
+    2, function(x) calc.chi(x, sum(miss.mult4)))
+hist(vec, col='gray'); abline(v=tt)
+plot(density(vec), type='l'); abline(v = tt)
+mean(vec >= tt)
 
-
-factorial(sum(miss.mult4)) * prod(((1/p)^(miss.mult4))/factorial(miss.mult4))
-
-###
+### test of independence of two subvectors (p.269)
+lam.to.f = function(lam, p, nu.h, nu.e){
+    w = nu.e + nu.h - 0.5*(p+nu.h+1)
+    t = sqrt((p^2 * nu.h^2 - 4)/(p^2 + nu.h^2 - 5))
+    df1 = p * nu.h
+    df2 = w*t - 0.5*(p*nu.h -2)
+    F.stat = (lam^(-1/t) - 1) * df2/df1
+#   return (c("F"=F.stat, "crit"=qf(1-0.95, df1, df2, lower.tail = FALSE)))
+    return (c("F"=F.stat, "p-val"=pf(F.stat, df1, df2, lower.tail = FALSE)))
+    }
 des2 <- cbind(max_2,miss.mat2)
 des4 <- cbind(max_4,miss.mat4)
+des4 = des4[,-13]
+
+S2 = var(des2)
+R2 = cor(des2)
+(lam2 = det(S2) / (det(S2[1:8,1:8]) * det(S2[9:16,9:16])))
+lam.to.f(lam2, 8, 8, n1 - 1 - 8)
+S4 = var(des4)
+R4 = cor(des4)
+(lam4 = det(S4) / (det(S4[1:8,1:8]) * det(S4[9:15,9:15])))
+lam.to.f(lam4, 8, 7, n2 - 1 - 7)
+
 
 m1 <- glm(a1 ~ .,family="binomial",data=as.data.frame(des2[,2:9]))
 
