4bb4deb96c23fbfd7a6d191e117858b7c611b78a
mickey <mickey.a.warner@gmail.com>
2014-10-13 20:07:04 -0600

    yes

--- 651/mini_project/3/code.R
+++ 651/mini_project/3/code.R
@@ -1,6 +1,69 @@
 ### bootstrap importance
+library(truncnorm)
+dat = as.matrix(read.table("~/files/R/651/data/faculty.dat"))
+colnames(dat) <- rownames(dat) <- NULL
 
-# (take code from mp2)
+y = as.vector(dat)
+
+# inverse gamma density
+igpdf=function(x, a, b)
+	(b^a)/gamma(a)*x^(-a-1)*exp(-b/x)
+
+g = function(x, m = 5, s2 = 100, a = 2.5, b = 1.5){
+    require(truncnorm)
+    mu = x[1]
+    sig2 = x[2]
+    if (sig2 <= 0)
+        return (0)
+    prod(dtruncnorm(y, 1, 7, mu, sqrt(sig2)))*dnorm(mu, m, sqrt(s2)) *
+        igpdf(sig2, a, b)
+    }
+modes = c(5.78, 0.31)
+
+### multivariate t
+dW = function(x)
+    gamma((nu + 2)/2) / (gamma(nu/2)*nu*pi*sqrt(det(sigma)) *
+        (1+1/nu*t(x-modes) %*% solve(sigma) %*% (x-modes))^((nu+2)/2))
+rW = function(n){
+    chi = rchisq(n, nu)
+    L = chol(sigma)
+    z = matrix(rnorm(n * nrow(L)), n, 2)
+    (z %*% L) * sqrt(nu/chi) + matrix(rep(modes, n), n ,2,
+        byrow = TRUE)
+    }
+
+nu = 4
+sigma = matrix(c(0.24, 0.17, 0.17, 0.23), 2, 2)
+sigma = sigma/4.589
+
+theta = rW(1000000)
+
+g.val = apply(theta, 1, g)
+I.val = apply(theta, 1, dW)
+
+# estimate of c
+(const = mean(g.val / I.val))
+
+weights = (g.val / I.val) / const
+
+# bootstrap sample indices
+b.ind = sample(nrow(theta), nrow(theta), replace = TRUE,
+    prob = weights)
+
+# posterior draws
+b.draws = theta[b.ind,]
+
+dens = kde2d(b.draws[,1], b.draws[,2], n = 100)
+dens2 = kde2d(b.draws[,1], b.draws[,2], n = 25)
+dens3 = kde2d(b.draws[,1], b.draws[,2], n = 200)
+
+filled.contour(dens$x, dens$y, log(dens$z))
+filled.contour(dens2$x, dens2$y, log(dens2$z))
+filled.contour(dens3$x, dens3$y, log(dens3$z))
+
+plot(b.draws[1:10000,], pch=20)
+
+# expectations
 
 ### metropolis-hastings
 autotune = function(accept, target = 0.25, k = 2.5)
