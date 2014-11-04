5439dc50faa9a223e8ef0e5de8e18ce0f85a63e2
mickey <mickey.a.warner@gmail.com>
2014-11-04 15:02:00 -0700

    yes

--- 666/homework/4/code.R
+++ 666/homework/4/code.R
@@ -1,23 +1,39 @@
+rm(list=ls())
+# F approximation for wilks lambda distribution
+lam.to.f = function(lam, p, nu.h, nu.e){
+    w = nu.e + nu.h - 0.5*(p+nu.h+1)
+    t = sqrt((p^2 * nu.h^2 - 4)/(p^2 + nu.h^2 - 5))
+    df1 = p * nu.h
+    df2 = w*t - 0.5*(p*nu.h -2)
+    F.stat = (lam^(-1/t) - 1) * df2/df1
+#   return (c("F"=F.stat, "crit"=qf(1-0.95, df1, df2, lower.tail = FALSE)))
+    return (c("F"=F.stat, "p-val"=pf(F.stat, df1, df2, lower.tail = FALSE)))
+    }
+
 ##### Part 1
 X = read.table("~/files/R/666/data/PSAData.txt", header = TRUE)
 xname = c("Intercept", names(X))
 X = as.matrix(X)
 # use intercept? getting problems in calculating Sxx, Sxy, etc.
+# Yes, use the intercept in the regression, just don't include it
+# when doing the canonical correlation analysis
 X.1 = as.matrix(cbind(1, X))
+colnames(X.1) = xname
 
 Y = read.table("~/files/R/666/data/PSAContributions.txt", header = TRUE)
 yname = names(Y)
 Y = as.matrix(Y)
 
+p = ncol(Y)
+q = ncol(X)
 n = nrow(X)
 ybar = apply(Y, 2, mean)
 
 betahat = solve(t(X.1) %*% X.1) %*% t(X.1) %*% Y
 E = t(Y - X.1 %*% betahat) %*% (Y - X.1 %*% betahat)
 H = t(betahat) %*% t(X.1) %*% Y - n * ybar %*% t(ybar)
-
-evals = eigen(solve(E) %*% H)$values
-evals / sum(evals) # first 2 eigen values
+# the n * ybar %*% t(ybar) term is to subtract out the portion
+# due to the intercept
 
 ### using covariance (because why ever?)
 # Sxx = var(X)
@@ -28,6 +44,7 @@ evals / sum(evals) # first 2 eigen values
 # Sb = solve(Sxx) %*% Sxy %*% solve(Syy) %*% t(Sxy)
 
 ### using correlation
+R = cor(cbind(X, Y))
 Rxx = cor(X)
 Ryy = cor(Y)
 Rxy = cor(X, Y)
@@ -37,6 +54,15 @@ Ra = solve(Ryy) %*% t(Rxy) %*% solve(Rxx) %*% Rxy
 # related to the x's
 Rb = solve(Rxx) %*% Rxy %*% solve(Ryy) %*% t(Rxy)
 
+
+# 1: is there a significant relationship between x's and y's
+lam.to.f(det(R) / (det(Rxx) * det(Ryy)), p, q, n-1-q)
+# reject that there is no linear relationship between x's and y's
+
+# 2: what is the essential dimensionality of the relationship
+#    between x's and y's
+evals = eigen(solve(E) %*% H)$values
+evals / sum(evals) # first 2 eigen values
 eigen(Ra)$vectors[,1:2] # y
 eigen(Rb)$vectors[,1:2] # x
 # for eigen 1: small x6 is related to large y1 and y5
@@ -52,8 +78,6 @@ Hr = t(betahat) %*% t(X.1) %*% Y - t(Br) %*% t(Xr) %*% Y
 # wilks lambda for comparing the full and reduced models
 lam = det(E) / det(E + Hr) # distributed Lambda(p,h,n-q-1)
 h = 1 # number of remove variables
-p = ncol(Y)
-q = ncol(X)
 
 nu.h = h
 nu.e = n - q - 1
@@ -62,6 +86,7 @@ nu.e = n - q - 1
 F.stat = (1 - lam) / lam * (nu.e - p + 1) / p
 pf(F.stat, p, nu.e - p + 1, lower.tail = FALSE)
 # crazy small, reject that snitch (i.e., Pb add much)
+lam.to.f(lam, p, nu.h, nu.e)
 
 ##### Part 2
 dat = matrix(scan("~/files/R/666/data/bodyfat.txt"),
