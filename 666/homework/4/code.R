728b24304afc71f3bf72b1ea6fdfca85f364c102
mickey <mickey.a.warner@gmail.com>
2014-11-05 08:41:15 -0700

    yes

--- 666/homework/4/code.R
+++ 666/homework/4/code.R
@@ -36,12 +36,16 @@ H = t(betahat) %*% t(X.1) %*% Y - n * ybar %*% t(ybar)
 # due to the intercept
 
 ### using covariance (because why ever?)
-# Sxx = var(X)
-# Syy = var(Y)
-# Sxy = var(X, Y)
-# 
-# Sa = solve(Syy) %*% t(Sxy) %*% solve(Sxx) %*% Sxy
-# Sb = solve(Sxx) %*% Sxy %*% solve(Syy) %*% t(Sxy)
+Sxx = var(X)
+Syy = var(Y)
+Sxy = var(X, Y)
+
+Sa = solve(Syy) %*% t(Sxy) %*% solve(Sxx) %*% Sxy
+Sb = solve(Sxx) %*% Sxy %*% solve(Syy) %*% t(Sxy)
+
+a = eigen(Sa)$vector
+c = diag(sqrt(diag(Syy))) %*% a
+c[,1:2]
 
 ### using correlation
 R = cor(cbind(X, Y))
@@ -51,6 +55,7 @@ Rxy = cor(X, Y)
 
 # related to the y's
 Ra = solve(Ryy) %*% t(Rxy) %*% solve(Rxx) %*% Rxy
+ solve(Ryy, t(Rxy)) %*% solve(Rxx,Rxy)
 # related to the x's
 Rb = solve(Rxx) %*% Rxy %*% solve(Ryy) %*% t(Rxy)
 
@@ -63,7 +68,10 @@ lam.to.f(det(R) / (det(Rxx) * det(Ryy)), p, q, n-1-q)
 #    between x's and y's
 evals = eigen(solve(E) %*% H)$values
 evals / sum(evals) # first 2 eigen values
-eigen(Ra)$vectors[,1:2] # y
+
+eigen(Ra)$vectors[,1:2]/c[,1:2] # y
+c[,1:2]
+
 eigen(Rb)$vectors[,1:2] # x
 # for eigen 1: small x6 is related to large y1 and y5
 # for eigen 2: large x4 and small x6 corresponds with large y1 and small y5
@@ -99,6 +107,8 @@ Y = dat[,1:2]
 X = dat[,3:15]
 X.1 = cbind(1, X)
 
+p = ncol(Y)
+q = ncol(X.1)
 n = nrow(X)
 ybar = apply(Y, 2, mean)
 
@@ -122,14 +132,6 @@ Rb = solve(Rxx) %*% Rxy %*% solve(Ryy) %*% t(Rxy)
 eigen(Ra)$vectors[,1] # y
 as.numeric(eigen(Rb)$vectors[,1]) # x
 
-# backward selection that stuff
-lower.mod = lm(Y[,1] ~ 1, data = data.frame(X))
-upper.mod = lm(Y[,1] ~ ., data = data.frame(X))
-mod = step(lower.mod, scope = list(lower=lower.mod, upper=upper.mod),
-    k = log(n), direction = "both", data = data.frame(X))
-
-
-extractAIC(mod, k = log(n))
 
 # step function to work for mlm models, beginning with
 # only the intercept, ending with all column given in X
@@ -163,7 +165,7 @@ mw.step = function(start.index, y, x, method = "both",
         # for forward selection
         if (method == "forward" || method == "both"){
             for (check in var.out){
-                test.aic[check] = extractAIC(FUN(y ~ 0 + x[,c(var.in, check)]))[2L]
+                test.aic[check] = extractAIC(FUN(y ~ 0 + x[,c(var.in, check)]), k = k)[2L]
                 }
             if (min(test.aic) < aic){
                 var.out = var.out[-which(var.out == which.min(test.aic))]
@@ -185,10 +187,11 @@ mw.step = function(start.index, y, x, method = "both",
         # backward selection
         if (method == "backward" || method == "both"){
             temp.in = var.in
-            if (method == "both")   # don't check the most recently added variable
+            # don't check the most recently added variable (flag = 1 if none removed)
+            if (method == "both" && flag == 0)
                 temp.in = temp.in[-length(temp.in)]
             for (check in temp.in){
-                test.aic[check] = extractAIC(FUN(y ~ 0 + x[,var.in[-which(check == temp.in)]]))[2L]
+                test.aic[check] = extractAIC(FUN(y ~ 0 + x[,var.in[-which(check == temp.in)]]), k = k)[2L]
                 }
             if (min(test.aic) < aic){
                 var.in = var.in[-which(var.in == which.min(test.aic))]
@@ -215,10 +218,34 @@ mw.step = function(start.index, y, x, method = "both",
 #    FUN(y ~ 0 + x[,var.in])
     }
 
-out1 = mw.step(1:q, Y, X, k = log(n), method = "backward")
-out2 = mw.step(sample(q, sample(q, 1)), Y, X, k = log(n))
+# the index of the variables kept after backward selecting 
+index = sort(mw.step(1:q, Y, X.1, k = log(n), method = "backward"))
 
-sort(out1)
-sort(out2)
+### using the reduced set of variables
+X.1 = X.1[,index]
+X = X.1[,-1]
 
+p = ncol(Y)
+q = ncol(X.1)
+n = nrow(X)
+ybar = apply(Y, 2, mean)
 
+betahat = solve(t(X.1) %*% X.1) %*% t(X.1) %*% Y
+E = t(Y - X.1 %*% betahat) %*% (Y - X.1 %*% betahat)
+H = t(betahat) %*% t(X.1) %*% Y - n * ybar %*% t(ybar)
+
+evals = eigen(solve(E) %*% H)$values
+evals / sum(evals) # only the first eigen value
+
+# using correlation
+Rxx = cor(X)
+Ryy = cor(Y)
+Rxy = cor(X, Y)
+
+# related to the y's
+Ra = solve(Ryy) %*% t(Rxy) %*% solve(Rxx) %*% Rxy
+# related to the x's
+Rb = solve(Rxx) %*% Rxy %*% solve(Ryy) %*% t(Rxy)
+
+eigen(Ra)$vectors[,1] # y
+as.numeric(eigen(Rb)$vectors[,1]) # x
