e7458cc4ca3d368f2c80eb9a631b18f1e5959d3c
mickey <mickey.a.warner@gmail.com>
2014-11-15 13:08:05 -0700

    yes

--- 651/final_project/code/functions.R
+++ 651/final_project/code/functions.R
@@ -1,11 +1,39 @@
-# logit function
+### logit function
 logit = function(x)
     log(x/(1-x))
-# covariance for simulator input (eq. 1)
-cov.simul = function(params, x, t){
-    lam.eta = params[1]
-    rho.x = params[2:(1+px)]
-    rho.t = params[(2+px):(2+pt)]
-
-
+### covariance(s) for simulator input (eq. 1)
+make.cov = function(y, rho, lam){
+    n = nrow(y)
+    out = diag(n)
+    for (i in 1:(n-1)){
+        for (j in (i+1):n){
+            out[i,j] <- out[j,i] <- prod(rho^(4*(y[i,] - y[j,])^2))
+            }
+        }
+    return (1/lam*out)
     }
+
+
+make2 = function(y, rho, lam){
+    n = nrow(y)
+    out = matrix(1, n, n)
+    for (i in 1:length(rho))
+        out = out * rho[i]^(4 * as.matrix(dist(y[,i]))^2)
+#   out = out + diag(n)
+    return (1/lam*out)
+    }
+
+# with initialized distances (should work for x's and t's, but not thetas)
+y.dist = rep(list(matrix(0, m, m)), p)
+for (i in 1:p)
+    y.dist[[i]] = 4*as.matrix(dist(y[,i]))^2
+
+make3 = function(y, rho, lam){
+    n = nrow(y)
+    out = matrix(1, n, n)
+    for (i in 1:length(rho))
+        out = out * rho[i]^y.dist[[i]]
+    return (1/lam*out)
+    }
+
+
