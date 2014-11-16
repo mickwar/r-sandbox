e8f569ee43855a3efdfb0c057a07bd76517a79f5
mickey <mickey.a.warner@gmail.com>
2014-11-16 09:20:29 -0700

    yes

--- 651/final_project/code/code.R
+++ 651/final_project/code/code.R
@@ -2,7 +2,7 @@ source("functions.R")
 field = matrix(scan("~/files/R/651/final_project/data/field.txt"), 9, 4, byrow=TRUE)
 simul = matrix(scan("~/files/R/651/final_project/data/simul.txt"), 693, 13, byrow=TRUE)
 
-# original data
+### original data
 field.x = field[,1]
 field.y = field[,2:4]
 
@@ -10,16 +10,31 @@ simul.x = simul[,1]
 simul.t = simul[,2:10]
 simul.y = simul[,11:13]
 
+# set constant variables
 n = nrow(field)
 m = nrow(simul)
 px = 1
 pt = ncol(simul.t)
 p = px + pt
+q = ncol(field.y)
 
-# transform the data
+### transform the data
 fdat.y = logit(field.y)
 sdat.y = logit(simul.y)
+y.mean = apply(sdat.y, 2, mean)
+y.sd = apply(sdat.y, 2, sd)
 
+# standardize output based on simulation mean and sd
+fdat.y = (fdat.y - matrix(rep(y.mean, n), n, q, byrow=TRUE)) /
+    matrix(rep(y.sd, n), n, q, byrow=TRUE)
+sdat.y = (sdat.y - matrix(rep(y.mean, m), m, q, byrow=TRUE)) /
+    matrix(rep(y.sd, m), m, q, byrow=TRUE)
+
+### ??
+K.eta = svd(t(sdat.y))$u
+apply(K.eta, 2, mean)
+apply(K.eta, 2, sd) # 1/sqrt(m-1)
+### ??
 
 xmin = min(field.x)
 xmax = max(field.x)
@@ -32,8 +47,4 @@ tmax = apply(simul.t, 2, max)
 sdat.t = simul.t - matrix(rep(tmin, m), m, pt, byrow=TRUE)
 sdat.t = sdat.t / (matrix(rep(tmax, m), m, pt, byrow=TRUE) - matrix(rep(tmin, m), m, pt, byrow=TRUE))
 sdat.xt = cbind(sdat.x, sdat.t)
-
-
-
-0.001^y.dist[[1]]
 
