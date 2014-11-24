344bd65b6e549170024940e888b1d5b2b310226b
mickey <mickey.a.warner@gmail.com>
2014-11-24 12:59:05 -0700

    yes

--- 666/final/code.R
+++ 666/final/code.R
@@ -1,39 +1,7 @@
-get.a.star = function(n, test.mu, size = 1000, p = 6, mu, sig2){
-    require(MASS)
-    out = matrix(0, n, p)
-    if (missing(mu))
-        mu = rep(0, p)
-    if (missing(sig2))
-        sig2 = diag(p)
-    for (i in 1:n){
-        x = mvrnorm(size, mu, sig2)
+library(ISLR)
+library(tree)
+library(randomForest)
 
-        # discriminant function
-        xbar = apply(x, 2, mean)
-        S = var(x)
-        a = solve(S) %*% (xbar - test.mu)
+dat = Hitters # Hitters is from ISLR package
 
-        out[i,] = diag(sqrt(diag(S))) %*% a
-        }
-    return (out)
-    }
-
-n = 10000
-size = 500
-p = 7
-test.mu = c(1, 0.5, -0.5, 0, 0, 0, 0)
-mu = rep(0, p)
-sig2 = diag(p)
-x = get.a.star(n, test.mu, size, p, mu, sig2)
-
-
-plot(density(x[,1]), ylim=c(0, 10), xlim=c(-1.3, .3))
-for (i in 1:ncol(x))
-    points(density(x[,i]), type='l', col=i)
-
-(ms = apply(x, 2, mean))
-(ss = apply(x, 2, sd))
-
-for (i in 1:ncol(x))
-    curve(dnorm(x, ms[i], ss[i]), add = TRUE, lwd=3, col=i)
-
+tree(dat[,19] ~ dat[,c(2,7)])
