75b172595f5adfbea7b5bbc0b6b591ac8a9f16d6
mickey <mickey.a.warner@gmail.com>
2014-12-01 15:45:49 -0700

    yes

--- mcmc/bayes_functions.R
+++ mcmc/bayes_functions.R
@@ -15,12 +15,15 @@
 # accept - the acceptance rate within a given window
 # target - the acceptance rate goal
 # k - the maximum the candidate sigma can change by, 1/k is 
-#     minimum it can change by. For instance, is the current
+#     minimum it can change by. For instance, if the current
 #     sigma is 2, and in some window of mcmc iterations every
 #     draw was accepted, the new candidate sigma would now
 #     be 2*k, which should serve to reduce the acceptance rate.
 #     On the other hand, if no draws are accepted, the new candidate
 #     sigma would then be 2/k. I recommend k = window/50
+#     Larger values of k will change sigma by a larger amount,
+#     and vice versa for smaller values (k -> 1, autotune() -> 1,
+#     everywhere)
 autotune = function(accept, target = 0.25, k = 2.5)
     (1+(cosh(accept-target)-1)*(k-1)/(cosh(target-
         ceiling(accept-target))-1))^sign(accept-target)
