1ae6561286fbc4893ae6a615a949b9b16c94c058
mickey <mickey.a.warner@gmail.com>
2014-12-01 21:25:50 -0700

    yes

---
+++ 666/final/read_data.R
@@ -0,0 +1,53 @@
+movie.dat = read.table("~/files/R/data/movie_data.txt", header=T)
+ben = read.table("~/files/R/data/movie_ben.txt", header=T)
+ben[,1] = as.character(ben[,1])
+movies = ben[,1]
+
+movie.dat[,1] = as.character(movie.dat[,1])
+movie.dat$MPAA = as.character(movie.dat$MPAA)
+
+n.var = ncol(movie.dat) - 1
+dat = data.frame(matrix(0, nrow(ben), n.var + ncol(ben)))
+dat[,1:2] = ben[,2:1]
+not_found = NULL
+for (i in 1:nrow(ben)){
+    j = which(ben[i,1] == movie.dat[,1])
+    if (length(j) == 0){
+        not_found[length(not_found)+1] = ben[i,1]
+    } else {
+        dat[i,3:(2+n.var)] = movie.dat[j,2:(n.var+1)]
+        }
+    }
+
+names(dat) = c("ben", names(movie.dat))
+
+dat = cbind(dat, "kids_I"=is.na(dat$kids_S)*1)
+for (i in 3:5)
+    dat[,i] = ifelse(is.na(dat[,i]), 0, dat[,i])
+
+# functions
+# if drop = TRUE, drop the last column
+mpaa.matrix = function(x){
+    level = c("PG13", "PG", "G")
+    # NR is the dropped level
+    n = length(x)
+    k = length(level)
+    out = matrix(0, n, k)
+    for (i in 1:k)
+        out[which(x == level[i]), i] = 1
+    return (out)
+    }
+Y = dat[,1]
+raw.X = as.matrix(cbind(dat[,3:5], mpaa.matrix(dat$MPAA),
+    dat[,7:15]))
+X = cbind(raw.X[,1:6], 100*(raw.X[,7]-raw.X[,9])/(raw.X[,7]+
+    raw.X[,8]-raw.X[,9]-raw.X[,10]), 100*raw.X[,9]/(raw.X[,9]+ 
+    raw.X[,10]), raw.X[,11:15])
+
+# top critic rotten tomatoes indicator
+X = cbind(X, "rot_I"=is.na(X[,8])*1)
+X[,8] = ifelse(is.na(X[,8]), 0, X[,8])
+
+var.names = c(names(dat)[3:5], as.character(unique(dat[,6])[1:3]),
+    "rot_crit", "rot_top", names(dat)[11:15], "rot_I")
+colnames(X) = var.names
