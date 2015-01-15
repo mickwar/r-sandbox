70ee1f7793606b318c0c7f7e290913dd141d3761
mickey <mickey.a.warner@gmail.com>
2015-01-15 15:50:30 -0700

    yes

--- 637/homework/2/code.R
+++ 637/homework/2/code.R
@@ -4,18 +4,22 @@ X = cbind(1, log(1:length(y)))
 
 
 ### part a
+pdf("figs/data_plot.pdf", width = 9, height = 9)
 plot(y, pch=20, xaxt = "n", ylab = "Number of Cases of AIDS",
-    xlab = "Time period", lwd=5)
+    xlab = "Time period", lwd=5, ylim=c(0,max(y)+10))
 abline(v=c(4.5, 8.5, 12.5, 16.5), lty=2)
 axis(1, at = 1:20, labels = paste0(rep("Q", 20), 1:4))
 for (i in 4:8)
-    text(4*(i-3)-2+0.5, max(y), paste0("198", i), cex = 2.5)
+    text(4*(i-3)-2+0.5, max(y)+10, paste0("198", i), cex = 2.5)
+dev.off()
 
 ### part b
-plot(log(1:20), log(y), pch=20, xaxt = "n", ylab = "Number of Cases of AIDS",
-    xlab = "Time period")
+pdf("figs/log_data.pdf", width = 9, height = 9)
+plot(log(1:20), log(y), pch=20, xaxt = "n", ylab = "log Number of Cases of AIDS",
+    xlab = "log Time period")
 abline(v=log(c(4.5, 8.5, 12.5, 16.5)), lty=2)
 axis(1, at = log(1:20), labels = paste0(rep("Q", 20), 1:4))
+dev.off()
 
 ### part c
 beta = c(0, 1)  # starting point
@@ -28,7 +32,7 @@ while (diff > eps){
     oldbeta = beta
     xbeta = X %*% oldbeta
     W = diag(as.numeric(exp(xbeta)), length(y))
-    z = xbeta + (y - exp(xbeta)) * exp(-xbeta)
+    z = xbeta + y * exp(-xbeta) - 1
     beta = solve(t(X) %*% W %*% X) %*% t(X) %*% W %*% z
     diff = sqrt(sum((beta - oldbeta)^2))
     if (TRUE){
@@ -41,5 +45,15 @@ while (diff > eps){
 ### part d
 summary(glm(y ~ 0 + X, family = poisson))
 
-
 ### part e
+# covariance matrix (using the latest W matrix)
+solve(t(X) %*% W %*% X)
+
+# standard errors
+(se = sqrt(diag(solve(t(X) %*% W %*% X))))
+
+# z-statistics for beta[2] (slope parameter, for x_i = log(i))
+beta[2] / se[2]
+
+# confidence intervals
+cbind(beta - qnorm(0.975) * se, beta + qnorm(0.975) * se)
