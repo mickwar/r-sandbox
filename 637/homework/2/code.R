02ef86805de0ffe41464e64520488eced6cd32ae
mickey <mickey.a.warner@gmail.com>
2015-01-15 12:14:21 -0700

    yes

--- 637/homework/2/code.R
+++ 637/homework/2/code.R
@@ -5,11 +5,11 @@ X = cbind(1, log(1:length(y)))
 
 ### part a
 plot(y, pch=20, xaxt = "n", ylab = "Number of Cases of AIDS",
-    xlab = "Time period")
+    xlab = "Time period", lwd=5)
 abline(v=c(4.5, 8.5, 12.5, 16.5), lty=2)
 axis(1, at = 1:20, labels = paste0(rep("Q", 20), 1:4))
 for (i in 4:8)
-    text(4*(i-3)-2, max(x), paste0("198", i), cex = 2.5)
+    text(4*(i-3)-2+0.5, max(y), paste0("198", i), cex = 2.5)
 
 ### part b
 plot(log(1:20), log(y), pch=20, xaxt = "n", ylab = "Number of Cases of AIDS",
