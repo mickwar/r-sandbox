addb72111ff62f3e492d3ffbf5ef05eb6a390637
mickey <mickey.a.warner@gmail.com>
2015-02-04 09:52:39 -0700

    yes

--- 637/homework/5/code.R
+++ 637/homework/5/code.R
@@ -7,12 +7,12 @@ li = c(8, 8, 10, 10, 12, 12, 12, 14, 14, 14, 16, 16, 16, 18, 20, 20, 20, 22, 22,
 y = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0)
 cbind(y, li)
 
-
 mod = glm(y ~ li, family = binomial(link = "logit"))
 summary(mod)
 exp(coef(mod))
 
-phat = logistic(cbind(1, li) %*% coef(mod))
+phat = logistic(cbind(1, li) %*% coef(mod)) # only for logit link
+# phat = predict(mod, type="response") # for other links
 
 p0 = seq(1, 0, length = 101)
 sens = double(length(p0))
@@ -22,7 +22,15 @@ for (i in 1:length(p0)){
     sens[i] = sum(y * yhat) / sum(y)
     spec[i] = sum((1-y) * (1-yhat)) / sum(1-y)
     }
-cbind(1-spec, sens, p0)
+
+### sensitivty at p0 = 0.5 (p0[51] = 0.5)
+yhat = ifelse(phat >= p0[51], 1, 0)
+# sens
+c(sum(y * yhat), sum(y))
+sum(y * yhat) / sum(y)
+# spec
+c(sum((1-y) * (1-yhat)), sum(1-y))
+sum((1-y) * (1-yhat)) / sum(1-y)
 
 calc_auc = function(sens, spec){
     y = sens
@@ -35,7 +43,9 @@ calc_auc = function(sens, spec){
     }
 calc_auc(sens, spec)
 
+pdf("./roc.pdf")
 plot(1-spec, sens, type='l', main = paste0("ROC Curve -- AUC = ",
-    round(calc_auc(sens, spec),3)),
+    round(calc_auc(sens, spec),3)), lwd = 2,
     xlab = "1 - Specificity", ylab = "Sensitivity")
-lines(c(0,1), c(0,1), lty=2, col='gray', lwd=2)
+lines(c(0,1), c(0,1), lty=2, col='darkgray', lwd=2)
+dev.off()
