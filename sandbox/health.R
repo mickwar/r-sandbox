77db8a1841bce41bba43f76c9d3b8c8c4d4e87a2
mickey <mickey.a.warner@gmail.com>
2015-02-17 12:25:23 -0700

    yes

--- sandbox/health.R
+++ sandbox/health.R
@@ -3,7 +3,11 @@ dat = read.table("~/files/data/health.txt", header=TRUE)
 y = ifelse(dat$sick == "none", 0, 1)
 x = dat[,2:ncol(dat)]
 
-mod = glm(y ~ 0 + time, data = x, family = binomial)
+int.mod = glm(y ~ 1, data = x, family = binomial)
+full.mod = glm(y ~ ., data = x, family = binomial)
 summary(mod)
 
 apply(x, 2, sum)
+
+mod = step(int.mod, scope = list("lower"=int.mod, "upper"=full.mod), k = log(length(y)))
+
