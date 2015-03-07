07952008e7e0094088f16f9334f48a15bc2c7fe7
mickey <mickey.a.warner@gmail.com>
2015-03-06 23:02:06 -0700

    yes

--- 637/homework/8/code.R
+++ 637/homework/8/code.R
@@ -1,4 +1,6 @@
 source("~/files/R/mcmc/bayes_functions.R")
+library(MASS)
+library(truncnorm)
 
 load("~/files/data/casella.Rdata")
 
@@ -13,82 +15,70 @@ new.train[is.na(new.train)] = 0
 
 n = NROW(new.train)
 Y = new.train$stars.scores
-temp = matrix(0, n, 5)
-for (i in 1:n)
-    temp[i, Y[i]] = 1
-Y = temp
-rm(temp)
+J = max(Y)
+# temp = matrix(0, n, 5)
+# for (i in 1:n)
+#     temp[i, Y[i]] = 1
+# Y = temp
+# rm(temp)
 
 X = cbind(1, new.train$help.yes, new.train$help.total, new.train$year)
+p = ncol(X)
+matrix(scale(X), n, p)
+X = matrix(scale(X), n, p)
+X[,1] = 1
+
+xtx = solve(t(X) %*% X)
+xtxx = xtx %*% t(X)
 
 
-calc.post = function(param.vec, like.only = FALSE){
-    # likelihood
-    px = pnorm(X %*% param.vec)
-    if (any(px == 0 | px == 1)) # when likelihood is 0
-        return (-Inf)
-    out = sum(y*log(px) + (1-y)*log(1-px))
-    # priors (beta ~ N(0, 1))
-    if (!like.only)
-        out = out + sum(dnorm(param.vec, beta.mean, beta.var, log = TRUE))
-    return (out)
-    }
+### parameters
+nburn = 100
+ngibbs = 1000
 
-### priors
-beta.mean = c(0, 0, 0)
-beta.cov = diag(3)
-beta.var = diag(beta.cov)
+param.beta = matrix(0, p, nburn + ngibbs)
+param.z = matrix(0, n, nburn + ngibbs)
+param.gamma = matrix(0, J-1, nburn + ngibbs)
+param.gamma[,1] = sort(runif(J-1, -2, 2))
 
+z.lower = double(n)
+z.upper = double(n)
 
-### MCMC part
-### initialize mcmc settings
-nburn = 25000
-nmcmc = 100000
-window = 1000
-nparams = 3
-params = matrix(0, nburn + nmcmc, nparams)
-accept = matrix(0, nburn + nmcmc, nparams)
-lower = rep(-Inf, 3)
-upper = rep(Inf, 3)
+### gibbs sampling
+for (i in 2:(nburn + ngibbs)){
+    cat("Iteration:", i, "/", nburn + ngibbs, "\n")
+    # beta
+    param.beta[,i] = mvrnorm(1, xtxx %*% param.z[,i-1], xtx)
 
-sigs = rep(1, nparams)
-sigs = c(1.0878, 0.0293, 1.5996)
-
-params[1,] = rep(0, 3)
-post = calc.post(params[1,])
-cand.param = params[1,]
-post.mat = matrix(-Inf, nburn + nmcmc, nparams)
-post.mat[1, 1] = post
-
-for (i in 2:(nburn+nmcmc)){
-#   cat("\rIteration",i,"/",nburn+nmcmc)
-    params[i,] = params[i-1,]
-    for (j in 1:nparams){
-        cand = rnorm(1, params[i,j], sigs[j])
-        if (cand >= lower[j] && cand <= upper[j]){
-            cand.param[j] = cand
-            cand.post = calc.post(cand.param)
-            # check whether to accept draw or not
-            if (log(runif(1)) < cand.post - post){
-                post = cand.post
-                params[i,j] = cand
-                accept[i,j] = 1
-            } else {
-                cand.param[j] = params[i,j]
-                }
+    # z
+    for (j in 1:n){
+        if (Y[j] == 1){
+            z.lower[j] = -Inf
         } else {
-            cand.param[j] = params[i,j]
+            z.lower[j] = param.gamma[Y[j]-1, i-1]
             }
-        post.mat[i, j] = post
+        if (Y[j] == J){
+            z.upper[j] = Inf
+        } else {
+            z.upper[j] = param.gamma[Y[j], i-1]
+            }
         }
-    # adjust the candidate sigma
-    if (floor(i/window) == i/window && i <= nburn)
-        sigs = sigs*autotune(apply(accept[(i-window+1):i,], 2,
-            mean), k = max(window/50, 1.1))
-#   if (i == (nburn+nmcmc))
-#       cat("\n")
+    param.z[,i] = rtruncnorm(1, mean = X %*% param.beta[,i], sd = 1,
+        a = z.lower, b = z.upper)
+
+    # gamma
+    z.uniq = sort(unique(c(z.lower, z.upper)))
+    for (j in 1:(J-1)){
+        param.gamma[j, i] = runif(1,
+            min = max(max(param.z[Y == j, i]), z.uniq[j]),
+            max = min(min(param.z[Y == j+1, i]), z.uniq[j+1]))
+        }
     }
 
-### remove burnin
-params = params[(nburn+1):(nburn+nmcmc),]
-accept = accept[(nburn+1):(nburn+nmcmc),]
+par(mfrow=c(3,1))
+plot(param.beta[1,], type='l')
+plot(param.z[2,], type='l')
+plot(param.gamma[4,], type='l')
+
+
+
