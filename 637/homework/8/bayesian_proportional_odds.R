bdbd793b9f602348867a1c2663d108e5d3671aa8
mickey <mickey.a.warner@gmail.com>
2015-03-11 08:00:31 -0600

    yes

--- 637/homework/8/bayesian_proportional_odds.R
+++ 637/homework/8/bayesian_proportional_odds.R
@@ -1,3 +1,4 @@
+library(splines)
 source("~/files/R/mcmc/bayes_functions.R")
 
 load("~/files/data/casella.Rdata")
@@ -9,31 +10,29 @@ new.train[is.na(new.train)] = 0
 n = NROW(new.train)
 Y = new.train$stars.scores
 J = max(Y)
-# temp = matrix(0, n, 5)
-# for (i in 1:n)
-#     temp[i, Y[i]] = 1
-# Y = temp
-# rm(temp)
-
-
-plot(X[, 2], Y, pch=20, col=Y)
-
 
 
 #X = cbind(1, new.train$help.yes, new.train$help.total, new.train$year)
-X = cbind(1, train$help.yes, train$help.total, train$year)
-X[is.na(X)] = 0
-Xmean = apply(X, 2, mean)
-Xsd = apply(X, 2, sd)
-Xmean[4] = 2007
-Xsd[4] = 1
-X = (X - matrix(Xmean, nrow(X), length(Xmean), byrow = TRUE)) /
-    matrix(Xsd, nrow(X), length(Xsd), byrow = TRUE)
-X[,1] = 1
-
-X = cbind(X, X[,2] * X[,3])
+# X = cbind(1, train$help.yes, train$help.total, train$year)
+# X[is.na(X)] = 0
+# Xmean = apply(X, 2, mean)
+# Xsd = apply(X, 2, sd)
+# Xmean[4] = 2007
+# Xsd[4] = 1
+# X = (X - matrix(Xmean, nrow(X), length(Xmean), byrow = TRUE)) /
+#     matrix(Xsd, nrow(X), length(Xsd), byrow = TRUE)
+# X[,1] = 1
+# 
+# X = cbind(X, X[,2] * X[,3], ns(X[,2] / X[,3],
+#     knots=seq(0.1, 0.9, by=0.2)))
 # X = cbind(1, train$help.yes, train$help.total, train$help.yes*train$help.total,
 #     train$year, train$year^2)
+X = cbind(1, train$help.yes/train$help.total)#, ns(train$year,
+    #knots = c(2002, 2007, 2013)))
+X[is.na(X)] = 0
+#X = cbind(X[,c(1,3)], ns(X[,2], knots = seq(0.2, 0.8, length = 3)))
+X = cbind(1, ns(X[,2], df= 4))
+
 p = ncol(X)
 
 
@@ -45,12 +44,20 @@ n.test = NROW(new.test)
 Y.test = new.test$stars.scores
 
 #X.test = cbind(1, new.test$help.yes, new.test$help.total, new.test$year)
-X.test = cbind(1, test$help.yes, test$help.total, test$year)
+# X.test = cbind(1, test$help.yes, test$help.total, test$year)
+# X.test[is.na(X.test)] = 0
+# X.test = (X.test - matrix(Xmean, nrow(X.test), length(Xmean), byrow = TRUE)) /
+#     matrix(Xsd, nrow(X.test), length(Xsd), byrow = TRUE)
+# X.test[,1] = 1
+# X.test = cbind(X.test, X.test[,2] * X.test[,3],
+#     ns(X.test[,2] / X.test[,3], knots = seq(0.1, 0.9, by = 0.2)))
+
+X.test = cbind(1, test$help.yes/test$help.total)#, ns(test$year,
+    #knots = c(2002, 2007, 2013)))
 X.test[is.na(X.test)] = 0
-X.test = (X.test - matrix(Xmean, nrow(X.test), length(Xmean), byrow = TRUE)) /
-    matrix(Xsd, nrow(X.test), length(Xsd), byrow = TRUE)
-X.test[,1] = 1
-X.test = cbind(X.test, X.test[,2] * X.test[,3])
+#X.test = cbind(X.test[,c(1,3)], ns(X.test[,2],
+#    knots = seq(0.2, 0.8, length = 3)))
+X.test = cbind(1, ns(X.test[,2], df=4))
 
 ### compute log posterior
 calc.post = function(params){
@@ -90,18 +97,19 @@ calc.post = function(params){
 beta.sd = 10
 
 
-nburn = 5000
-nmcmc = 10000
+nburn = 10000
+nmcmc = 20000
 window = 500
 # nparams = (J-1) * p # cumulative logit
-nparams = (J-1) + (p-1) # porportional
+# nparams = (J-1) + (p-1) # porportional
+nparams = (J-1) + (p-1) # proportional (with spline)
 params = matrix(0, nburn + nmcmc, nparams)
 accept = matrix(0, nburn + nmcmc, nparams)
 lower = rep(-Inf, nparams)
 upper = rep(Inf, nparams)
 params[1, 1:4] = seq(0, 1, length=4)
 
-sigs = rep(0.001, nparams)
+sigs = rep(0.1, nparams)
 # sigs = c(1.2340859, 2.4084625, 1.9771273, 1.5098575, 0.6543709, 1.2939921, 1.1159381,
 #     0.7045175, 0.5206961, 1.5336611, 1.3352886, 0.6416922, 0.6122230, 2.2898420,
 #     1.9232403, 0.8846596)
@@ -178,7 +186,7 @@ post.pred = function(params, data){
     X = data$X
     n = nrow(X)
     mse = double(nrow(params))
-    out = matrix(0, nrow(params), length(Y.test))
+    out = matrix(0, nrow(params), length(Y))
     for (i in 1:nrow(params)){
         temp.p = matrix(0, n, J)
         for (j in 1:(J-1)){
@@ -208,8 +216,9 @@ pred = post.pred(params, list("Y"=Y.test, "X"=X.test))
 pred = post.pred(params, list("Y"=Y, "X"=X))
 plot(density(pred$mse))
 calc.mode(density(pred$mse))
+mean(pred$mse)
 
-hist(pred$preds[,12])
+hist(pred$preds[,20])
 
 
 library(VGAM)
