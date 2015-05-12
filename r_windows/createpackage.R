8eb9657cb5f9127d8e36d5bd6b90ff1b617c752c
mickey <mickey.a.warner@gmail.com>
2015-05-12 15:15:58 -0600

    yes

--- r_windows/createpackage.R
+++
@@ -1,8 +0,0 @@
-default=getwd()
-setwd("C:/Users/Mickey/Desktop/Downloads/School/R Play/mickey")
-
-package.skeleton(name="betaBayes")
-
-packname="betaBayes"
-dir(paste0("C:/Users/Mickey/Desktop/Downloads/School/R Play/",packname,"/R"))
-setwd(default)
