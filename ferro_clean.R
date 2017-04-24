5f46be96f7092eec1f10f3d7c7f91856ce67c5fa
mickey <mickey.a.warner@gmail.com>
2017-04-24 06:25:50 -0700

    yes

--- ferro_clean.R
+++ ferro_clean.R
@@ -1,4 +1,4 @@
-### Simulated example
+library(mwBASE)
 intervals.est = function(Tu){
     if (length(Tu) == 0)
         return (1)
@@ -9,179 +9,6 @@ intervals.est = function(Tu){
         }
     return (out)
     }
-# bayes.est = function(Tu){
-#     # theta ~ Beta(a, b)
-#     pr.a = 1
-#     pr.b = N
-# 
-#     # q = p^theta ~ Beta(c, d)
-#     pr.c = 1
-#     pr.d = 1
-# 
-#     m1 = sum(Tu == 1)
-#     N = length(Tu) + 1
-# 
-#     k = 0:m1
-#     v = lchoose(m1, k) + lbeta(k + pr.a + N - 1 - m1 + 1, pr.b) + lbeta(k + pr.c + sum(Tu - 1), pr.d + N - 1 - m1)
-# 
-#     lchoose(m1, k)
-# 
-#     lbeta(k + pr.a + N - 1 - m1 + 1, pr.b)
-#     lbeta(0 + pr.a + N - 1 - m1 + 1, pr.b)
-# 
-#     lbeta(k + pr.c + sum(Tu - 1), pr.d + N - 1 - m1)
-#     lbeta(0 + pr.c + sum(Tu - 1), pr.d + N - 1 - m1)
-# 
-# 
-# #   v = v + (pr.d + N - 1 - m1)*log(0 + pr.c + sum(Tu - 1))
-# #   v = v - lchoose(m1, round(m1/2))
-#     v = v - lbeta(0 + pr.a + N - 1 - m1 + 1, pr.b)
-#     v = v - lbeta(0 + pr.c + sum(Tu - 1), pr.d + N - 1 - m1)
-# 
-# num = sum(((-1)^k)*exp(v))
-# 
-# (((-1)^k)*exp(v))
-# 
-#     logavec = sort(v[(k %% 2) == 0], decreasing = TRUE)
-#     logbvec = sort(v[(k %% 2) == 1], decreasing = TRUE)
-# 
-#     lsa = logavec[1] + log(1 + sum(exp(logavec[-1] - logavec[1])))
-#     lsb = logbvec[1] + log(1 + sum(exp(logbvec[-1] - logbvec[1])))
-# 
-#     num = lsa + log(1 - exp(lsb - lsa))
-# 
-#     v = lchoose(m1, k) + lbeta(k + pr.a + N - 1 - m1, pr.b) + lbeta(k + pr.c + sum(Tu - 1), pr.d + N - 1 - m1)
-# 
-# #   v = v + (pr.d + N - 1 - m1)*log(0 + pr.c + sum(Tu - 1))
-# #   v = v - lchoose(m1, round(m1/2))
-#     v = v - lbeta(0 + pr.a + N - 1 - m1 + 1, pr.b)
-#     v = v - lbeta(0 + pr.c + sum(Tu - 1), pr.d + N - 1 - m1)
-# 
-# den = sum(((-1)^k)*exp(v))
-# 
-# num/den
-# 
-# (((-1)^k)*exp(v))
-# 
-# 
-# ((N - m1 + 1) / (N + 3)) / ((sum(Tu) - 2*N + 3 + m1) / (sum(Tu) - N + 2))
-# 
-# 
-# B = 10000
-# rs = rbeta(B, N - m1 + 1, m1 + 2)
-# qs = rbeta(B, sum(Tu) -2*N+2+m1+1,N-m1)
-# 
-# mean(rs/qs)
-# 
-# plot(density(rs/qs))
-# lines(density(out$params), col = 'red')
-# 
-#     logavec = sort(v[(k %% 2) == 0], decreasing = TRUE)
-#     logbvec = sort(v[(k %% 2) == 1], decreasing = TRUE)
-# 
-#     lsa = logavec[1] + log(1 + sum(exp(logavec[-1] - logavec[1])))
-#     lsb = logbvec[1] + log(1 + sum(exp(logbvec[-1] - logbvec[1])))
-# 
-#     den = lsa + log(1 - exp(lsb - lsa))
-# 
-#     exp(num - den)
-# 
-#     sum(choose(m1, k)*((-1)^k)*beta(k + pr.c + sum(Tu - 1), pr.d + N - 1 - m1)*beta(k + pr.a + N - 1 - m1 + 1, pr.b)) / 
-#     sum(choose(m1, k)*((-1)^k)*beta(k + pr.c + sum(Tu - 1), pr.d + N - 1 - m1)*beta(k + pr.a + N - 1 - m1, pr.b))
-# 
-#     sum(choose(m1, k)*((-1)^k)*beta(k + pr.c + sum(Tu - 1), pr.d + N - 1 - m1)*beta(k + pr.a + N - 1 - m1 + 1, pr.b))
-#     sum(choose(m1, k)*((-1)^k)*beta(k + pr.c + sum(Tu - 1), pr.d + N - 1 - m1)*beta(k + pr.a + N - 1 - m1, pr.b))
-# 
-#     }
-
-
-
-library(mwBASE)
-
-
-# theta = 0.5
-# ww = -1/log(runif(n))
-# ksi = double(n)
-# ksi[1] = ww[1] / theta
-# for (i in 2:n)
-#     ksi[i] = max((1-theta)*ksi[i-1], ww[i])
-# 
-# plot(log(ksi), xlim = c(1, 200), type='o')
-
-
-theta.vec = seq(0.05, 0.95, length = 10)
-mm = double(length(theta.vec))
-vv = matrix(0, length(theta.vec), 2)
-ints.mat = matrix(0, 1, length(theta.vec))
-for (j in 1:length(theta.vec)){
-    set.seed(2)
-    theta = theta.vec[j]
-    n = 10000
-    ww = -1/log(runif(n))
-    x = double(n)
-    x[1] = ww[1] / theta
-    for (i in 2:n)
-        x[i] = max((1-theta)*x[i-1], ww[i])
-    plot(log(x), xlim = c(0, 100))
-    plot((x) )
-
-    u = 250
-
-    Tu = diff(which(x > u))
-
-    N = sum(x > u)
-    m1 = sum(Tu == 1)
-
-    calc.post = function(z, param){
-        theta = param[1]
-        p = param[2]
-        if (theta <= 0 || theta > 1)
-            return (-Inf)
-        if (p <= 0 || p >= 1)
-            return (-Inf)
-
-        # Likelihood (from Eq. 3)
-        out = m1 * log(1 - theta*p^theta) + (N - 1 - m1)*(log(theta) + log(1-p^theta)) +
-            theta*log(p)*sum(Tu - 1)
-
-        # Priors
-#       out = out + dbeta(p, 1/2, 1/2, log = TRUE)
-        return (out)
-        }
-    out = mcmc_sampler(z, calc.post, 2, nburn = 20000, chain_init = c(theta, 0.5))
-    mm[j] = mean(out$param[,1])
-    vv[j,] = quantile(out$param[,1], c(0.025, 0.975))
-
-    ints.mat[1,j] = intervals.est(Tu)
-
-#   ### "Bootstrap" sample for the intervals estimator
-#   ### Adds about 10 seconds for each iteration j
-#   for (k in 1:NROW(ints.mat)){
-#       ww = -1/log(runif(n))
-#       x = double(n)
-#       x[1] = ww[1] / theta
-#       for (i in 2:n)
-#           x[i] = max((1-theta)*x[i-1], ww[i])
-
-#       Tu = diff(which(x > u))
-#       ints.mat[k,j] = intervals.est(Tu)
-#       }
-    }
-
-
-offset = 0.0075
-qq = apply(ints.mat, 2, quantile, c(0.025, 0.975))
-plot(theta.vec, mm, ylim = range(qq), xlab = expression("True" ~ theta),
-    ylab = expression("Posterior" ~ theta))
-points(theta.vec, vv[,1], pch="-")
-points(theta.vec, vv[,2], pch="-")
-segments(x0 = theta.vec, y0 = vv[,1], x1 = theta.vec, y1 = vv[,2])
-points(theta.vec, mm, pch = 16)
-points(theta.vec+offset, colMeans(ints.mat), col = 'red')
-points(theta.vec+offset, qq[1,], pch="-", col = 'red')
-points(theta.vec+offset, qq[2,], pch="-", col = 'red')
-segments(x0 = theta.vec+offset, y0 = qq[1,], x1 = theta.vec+offset, y1 = qq[2,], col = 'red')
-abline(0, 1)
 
 
 
@@ -193,20 +20,18 @@ x = read.csv("~/files/repos/data/wooster_1893_2014.csv")
 
 # Get rid of the extra space
 y = gsub(" ", "", x[,6]) # end at 2014
-y = gsub(" ", "", x[1:32324,6]) # end at 1987
+y = gsub(" ", "", x[1:32324,6]) # and at 1987
 y = gsub(" ", "", x[1:37803,6]) # end at 2002
+y = gsub(" ", "", x[1:38168,6]) # end at 2003
 
 y = y[-which(y == ".")]  # remove missing data
 y = as.numeric(y)
 
-tail(x, 1)
-x[32324,]
-x[37803,]
-
 wooster = -y
 
-n = 1000
-theta = 0.05
+### Simulated example
+n = 900
+theta = 0.20
 ww = -1/log(runif(n))
 y = double(n)
 y[1] = ww[1] / theta
@@ -214,12 +39,12 @@ for (i in 2:n)
     y[i] = max((1-theta)*y[i-1], ww[i])
 wooster = y
 
-
 plot(tail(wooster, 2000), pch = 16)
 
 n = length(wooster)
 #uu = seq(0.5, 14.5, by = 1.0)
-uu = seq(quantile(wooster, 0.9), quantile(wooster, 0.99), length = 16)
+uu = seq(quantile(wooster, 0.85), quantile(wooster, 0.97), length = 16)
+#uu = c(uu, seq(quantile(wooster, 0.95), quantile(wooster, 0.999), length = 16))
 mm = double(length(uu))
 vv = matrix(0, length(uu), 2)
 a.vec = double(length(uu))
@@ -243,16 +68,15 @@ for (j in 1:length(uu)){
         # Likelihood (from Eq. 3)
         out = m1 * log(1 - theta*p^theta) + (N - 1 - m1)*(log(theta) + log(1-p^theta)) +
             theta*log(p)*sum(x - 1)
-        m1 * log(1 - theta*p^theta) + (N - 1 - m1)*(log(theta) + log(1-p^theta)) +
-            theta*log(p)*sum(x - 1)
-        (N - 1)*log(1-p^theta) + (N - 1)*log(theta) + theta*log(p)*sum(x - 1)
 
-#   theta*p^(x*theta) - theta*p^(theta*(x+1))
+#       m1 * log(1 - theta*p^theta) + (N - 1 - m1)*(log(theta) + log(1-p^theta)) +
+#           theta*log(p)*sum(x - 1)
+#       (N - 1)*log(1-p^theta) + (N - 1)*log(theta) + theta*log(p)*sum(x - 1)
 
         # Priors
 #       out = out + dbeta(theta, 1/2, 1/2, log = TRUE)
-        out = out + dbeta(theta, 1/2, 1/20, log = TRUE)
-        out = out + dbeta(p, 1/2, 1/2, log = TRUE)
+#       out = out + dbeta(theta, 1/2, 1/20, log = TRUE)
+#       out = out + dbeta(p, 1/2, 1/2, log = TRUE)
         return (out)
         }
     out = mcmc_sampler(Tu, calc.post, 2, nburn = 50000, nmcmc = 30000, chain_init = c(0.5, 0.5))
@@ -323,32 +147,9 @@ for (j in 1:length(uu)){
     lines(uu[1:j], ints.vv[1:j,1], lty = 3, col = 'red')
     lines(uu[1:j], ints.vv[1:j,2], lty = 3, col = 'red')
     axis(3,  at = ux, labels = N.vec)
-    abline(h = theta, lty = 2, col = 'blue')
+    if (exists("theta"))
+        abline(h = theta, lty = 2, col = 'blue')
 
     }
 
-
-
-
-range(a.vec)
-
-ints = sapply(uu, function(x) intervals.est(diff(which(wooster > x))))
-
-ux = seq(-10, 20, by = 2)
-N.vec = double(length(ux))
-for (i in 1:length(ux))
-    N.vec[i] = sum(wooster > ux[i])
-
-plot(uu, mm, ylim = c(0, 1), xlab = "Threshold", ylab = "Extremal Index",
-    bty = 'n', type='b')
-lines(uu, vv[,1], lty=2)
-lines(uu, vv[,2], lty=2)
-axis(3,  at = ux, labels = N.vec)
-points(uu, ints, col = 'red')
-
-# plot(uu, apply(ints.boot, 2, mean, na.rm = TRUE), ylim = c(0, 1), xlab = "Threshold",
-#     ylab = "Extremal Index", bty = 'n', type='b')
-# lines(uu, apply(ints.boot, 2, quantile, 0.025, na.rm = TRUE), lty=2)
-# lines(uu, apply(ints.boot, 2, quantile, 0.975, na.rm = TRUE), lty=2)
-# axis(3,  at = ux, labels = N.vec)
 
