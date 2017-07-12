58215d3d6a498375b3a7fa627ce07244ec57541a
mickey <mickey.a.warner@gmail.com>
2017-07-12 15:40:31 -0700

    yes

--- rstan/code_ex.R
+++ rstan/code_ex.R
@@ -34,29 +34,43 @@ curve(dbeta(x, 1, -1/ksi), col = 'red', add = TRUE)
 
 
 
-ksi = 0.50
-ex = rgp(500, 0, 1, ksi)
-dat = list("N" = N, "exceedances" = ex, "threshold" = u)
+set.seed(1)
+N = 500
+ksi = -0.50
+ex = rgp(N, 0, 1, ksi)
+dat = list("N" = N, "exceedances" = ex)
 
 
-nburn = 1000
-nmcmc = 500
-nchain = 1
-fit = stan(file = "./extreme.stan", data = dat,
-    iter = nburn + nmcmc, warmup = nburn, chains = nchain, control = list("adapt_delta" = 0.80))
+nburn = 10000
+nmcmc = 10000
+nchain = 8
+fit = stan(file = "./extreme2.stan", data = dat,
+    iter = nburn + nmcmc, warmup = nburn, chains = nchain, control = list("adapt_delta" = 0.95))
 
 # Number of divergent iterations, proportion of divergences
 sapply(get_sampler_params(fit, inc_warmup=FALSE), function(x) sum(x[,'divergent__']))
 sapply(get_sampler_params(fit, inc_warmup=FALSE), function(x) mean(x[,'divergent__']))
 
 ind = c(sapply(get_sampler_params(fit, inc_warmup=FALSE), function(x) which(x[,'divergent__'] == 1)))
-ind = 1
+#ind = 1
 
 params = extract(fit, permuted = TRUE)
 
 plot(params$ksi[-ind], params$sigma[-ind], col = 'blue',
     xlim = range(params$ksi), ylim = range(params$sigma))
-points(params$ksi[ind], params$sigma[ind], col = 'red')
+points(params$ksi[ind], params$sigma[ind], col = 'blue')
+points(mean(params$ksi), mean(params$sigma), col = 'red', pch = 15)
+points(-0.5, 1, pch = 16, col = 'green')
+
+plot(cumsum(params$ksi)/(1:nmcmc))
+abline(h = -0.5)
+
+plot(cumsum(params$sigma)/(1:nmcmc))
+abline(h = 1)
+
+mean(params$ksi)
+mean(params$sigma)
+plot(params
 
 
 plot(params$theta[,1], xlim = c(1, 100))
