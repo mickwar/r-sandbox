d883ed2d5888b1350c548c260eda4a548452fa90
mickey <mickey.a.warner@gmail.com>
2017-07-14 11:05:45 -0700

    yes

--- rstan/code_ex.R
+++ rstan/code_ex.R
@@ -14,45 +14,40 @@ for (i in 2:n)
     y[i] = max((1-theta)*y[i-1], ww[i])
 plot(y)
 
+
 y = rnorm(n)
 u = quantile(y, 0.95)
+
+
 
 ex = y[y > u] - u
 N = length(ex)
 
 
-ex = rgp(100000, 0, 1, -1.5)
-
-
-ksi = -0.5
-ex = rgp(500, 0, 1, ksi)
-
-z = -ksi*ex / 1
-plot(density(z))
-curve(dbeta(x, 1, -1/ksi), col = 'red', add = TRUE)
-
-
-
 
 set.seed(1)
 N = 500
-ksi = -0.50
-ex = rgp(N, 0, 1, ksi)
+ksi = 0.0
+sigma = 1
+ex = rgp(N, 0, sigma, ksi)
+ex = sort(ex, decreasing = TRUE)
 dat = list("N" = N, "exceedances" = ex)
 
 
-nburn = 10000
-nmcmc = 10000
-nchain = 8
-fit = stan(file = "./extreme2.stan", data = dat,
-    iter = nburn + nmcmc, warmup = nburn, chains = nchain, control = list("adapt_delta" = 0.95))
+nburn = 1000
+nmcmc = 1000
+nchain = 1
+fit = stan(file = "./extreme.stan", data = dat,
+    iter = nburn + nmcmc, warmup = nburn, chains = nchain,
+    init = list(list("ksi" = 0.0, "sigma" = 1)),
+    control = list("adapt_delta" = 0.80))
 
 # Number of divergent iterations, proportion of divergences
 sapply(get_sampler_params(fit, inc_warmup=FALSE), function(x) sum(x[,'divergent__']))
 sapply(get_sampler_params(fit, inc_warmup=FALSE), function(x) mean(x[,'divergent__']))
 
 ind = c(sapply(get_sampler_params(fit, inc_warmup=FALSE), function(x) which(x[,'divergent__'] == 1)))
-#ind = 1
+ind = 1
 
 params = extract(fit, permuted = TRUE)
 
@@ -60,7 +55,7 @@ plot(params$ksi[-ind], params$sigma[-ind], col = 'blue',
     xlim = range(params$ksi), ylim = range(params$sigma))
 points(params$ksi[ind], params$sigma[ind], col = 'blue')
 points(mean(params$ksi), mean(params$sigma), col = 'red', pch = 15)
-points(-0.5, 1, pch = 16, col = 'green')
+points(ksi, sigma, pch = 16, col = 'green')
 
 plot(cumsum(params$ksi)/(1:nmcmc))
 abline(h = -0.5)
--- rstan/extreme.stan
+++ rstan/extreme.stan
@@ -1,23 +1,31 @@
 data {
   int<lower=0> N;
-  real exceedances[N];
+  vector[N] exceedances;
 }
 parameters {
-  real ksi; 
-  real<lower=0> sigma;
+  real ksi_raw; 
+  real<lower=0> sigma_raw;
 }
 transformed parameters {
-  real z[N];
-  for (i in 1:N)
-    z[i] = -exceedances[i]*ksi/sigma;
+    vector[2] sig_adj;
+    real ksi;
+    real sigma;
+    vector[N] ex_scaled;
+
+    ksi = 0.01;
+    sig_adj[1] = -ksi*max(exceedances);
+    sig_adj[2] = 0.0;
+    sigma = sigma_raw + max(sig_adj);
+    ksi = ksi_raw - sigma / max(exceedances);
+    ex_scaled = -ksi/sigma * exceedances;
 }
 model {
   ksi ~ normal(0, 1);
   sigma ~ inv_gamma(1, 1);
   if (ksi > 0)
-    exceedances ~ pareto_type_2(0, sigma/ksi, 1/ksi);
+    target += pareto_type_2_lpdf(exceedances | 0, sigma/ksi, 1/ksi);
   else if (ksi == 0)
-    exceedances ~ exponential(1/sigma);
-  else
-    z ~ beta(1, -1/ksi);
+    target += exponential_lpdf(exceedances | 1/sigma);
+  else if (ksi < 0)
+    target += beta_lpdf(ex_scaled | 1, -1/ksi);
 }
